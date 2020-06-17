b7c008222844a9651d8db9cddb9f05d1d02faf47
Markham White <markham@afterdarkdesigns.com>
2020-06-17 13:10:58 -0400

    Initial Commit

---
+++ Source Control/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Source Control
+//
+//  Created by Markham White on 6/17/20.
+//  Copyright © 2020 Markham White. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
