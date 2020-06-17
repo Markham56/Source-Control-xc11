57630ff308f84228899e4167505b54a613b00468
Markham White <markham@afterdarkdesigns.com>
2020-06-17 14:35:59 -0400

    Changed print to: print("from conflictbranch1")

--- Source Control/ViewController.swift
+++ Source Control/ViewController.swift
@@ -23,7 +23,7 @@ class ViewController: NSViewController {
      - returns: The sum of num1 and num2.
      */
     func addNumbers(num1: Int, num2: Int) -> Int {
-        print("from master branch")
+        print("from conflictbranch1")
         return num1 + num2
     }
     
